0fb5d85f091a4f88fa67ebe352470137ffa4f724
john <maquistemas@gmail.com>
2018-08-08 10:48:31 -0500

    segundo commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+/*jQuer - moment*/
+import * as $ from 'jquery';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
+
+ngOnInit(){
+
+}
+
+
+
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+/**Librerias */
+import { MomentModule } from 'angular2-moment';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MomentModule
   ],
   providers: [],
   bootstrap: [AppComponent]
